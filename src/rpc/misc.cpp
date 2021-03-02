ff34c35df851d826b5e59d473783fa65dc9251ad
Bedri Özgür Güler <bedriguler@gmail.com>
2021-03-02 18:14:22 +0300

    Removes zSAPPsupply from getinfo

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -69,18 +69,6 @@ UniValue getinfo(const JSONRPCRequest& request)
             "  \"difficulty\": xxxxxx,         (numeric) the current difficulty\n"
             "  \"testnet\": true|false,        (boolean) if the server is using testnet or not\n"
             "  \"moneysupply\" : \"supply\"    (numeric) The money supply when this block was added to the blockchain\n"
-            "  \"zSAPPsupply\" :\n"
-            "  {\n"
-            "     \"1\" : n,            (numeric) supply of 1 zSAPP denomination\n"
-            "     \"5\" : n,            (numeric) supply of 5 zSAPP denomination\n"
-            "     \"10\" : n,           (numeric) supply of 10 zSAPP denomination\n"
-            "     \"50\" : n,           (numeric) supply of 50 zSAPP denomination\n"
-            "     \"100\" : n,          (numeric) supply of 100 zSAPP denomination\n"
-            "     \"500\" : n,          (numeric) supply of 500 zSAPP denomination\n"
-            "     \"1000\" : n,         (numeric) supply of 1000 zSAPP denomination\n"
-            "     \"5000\" : n,         (numeric) supply of 5000 zSAPP denomination\n"
-            "     \"total\" : n,        (numeric) The total supply of all zSAPP denominations\n"
-            "  }\n"
             "  \"keypoololdest\": xxxxxx,      (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool\n"
             "  \"keypoolsize\": xxxx,          (numeric) how many new keys are pre-generated\n"
             "  \"unlocked_until\": ttt,        (numeric) the timestamp in seconds since epoch (midnight Jan 1 1970 GMT) that the wallet is unlocked for transfers, or 0 if the wallet is locked\n"
@@ -148,15 +136,6 @@ UniValue getinfo(const JSONRPCRequest& request)
     }
 
     obj.push_back(Pair("moneysupply",ValueFromAmount(nMoneySupply)));
-    UniValue zpivObj(UniValue::VOBJ);
-    for (auto denom : libzerocoin::zerocoinDenomList) {
-        if (mapZerocoinSupply.empty())
-            zpivObj.push_back(Pair(std::to_string(denom), ValueFromAmount(0)));
-        else
-            zpivObj.push_back(Pair(std::to_string(denom), ValueFromAmount(mapZerocoinSupply.at(denom) * (denom*COIN))));
-    }
-    zpivObj.push_back(Pair("total", ValueFromAmount(GetZerocoinSupply())));
-    obj.push_back(Pair("zSAPPsupply", zpivObj));
 
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
